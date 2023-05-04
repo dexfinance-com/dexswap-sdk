a93649fd05c54f5d5c1f33775aec9d3d39a457fb
badgerdf <badger@gordian.agency>
2023-05-04 16:43:40 +0300

    add dist

---
+++ dist/constants.d.ts
@@ -0,0 +1,42 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | number | string;
+export declare enum ChainId {
+    MAINNET = 42161,
+    TESTNET = 421613
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "";
+export declare const FACTORY_ADDRESS_MAP: {
+    42161: string;
+    421613: string;
+};
+export declare const INIT_CODE_HASH = "";
+export declare const INIT_CODE_HASH_MAP: {
+    42161: string;
+    421613: string;
+};
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
